b265104d44ffb0b644915c4bf60e10d613bde1b7
Jean-Sebastien Legare <js@sitelabs.ca>
2020-07-22 01:39:18 -0700

    main.js

---
+++ docs/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var myGeoJSONPath = './countries.geo.json';
+var myCustomStyle = {
+    stroke: true,
+    fill: true,
+    fillColor: '#fff',
+    fillOpacity: 1
+}
+
+$.getJSON(myGeoJSONPath,function(data){
+    var map = L.map('map').setView([39.74739, -105], 4);
+
+    // https://github.com/Leaflet/Leaflet.fullscreen
+    map.addControl(new L.Control.Fullscreen({
+	title: {
+	    'false': 'View Fullscreen',
+	    'true': 'Exit Fullscreen'
+	}
+    }));
+
+    map.on('fullscreenchange', function () {
+	if (map.isFullscreen()) {
+	    console.log('entered fullscreen');
+	} else {
+	    console.log('exited fullscreen');
+	}
+    });
+
+    L.geoJson(data, {
+	clickable: false,
+	style: myCustomStyle
+    }).addTo(map);
+
+})
+</script>
